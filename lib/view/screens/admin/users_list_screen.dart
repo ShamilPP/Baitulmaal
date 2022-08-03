708df909b44b4b65d9a6c937bdb1b7f9671e3c4b
ShamilPP <shamilpp4115@gmail.com>
2022-08-03 09:54:09 +0530

    users list screen updated

--- lib/view/screens/admin/users_list_screen.dart
+++ lib/view/screens/admin/users_list_screen.dart
@@ -1,12 +1,11 @@
 import 'package:baitulmaal/model/user_model.dart';
-import 'package:baitulmaal/utils/colors.dart';
-import 'package:baitulmaal/view/animations/slide_animation.dart';
-import 'package:baitulmaal/view/screens/sign_up_screen.dart';
 import 'package:baitulmaal/view_model/admin_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../animations/slide_animation.dart';
 import '../../widgets/users_list.dart';
+import '../sign_up_screen.dart';
 
 class UsersScreen extends StatefulWidget {
   const UsersScreen({Key? key}) : super(key: key);
@@ -36,77 +35,86 @@ class _UsersScreenState extends State<UsersScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Stack(
-        children: [
-          Column(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 200,
-                color: primaryColor,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SlideAnimation(
-                      delay: 400,
-                      child: Text(
-                        'Users',
-                        style: TextStyle(fontSize: 40, color: Colors.white, fontWeight: FontWeight.w300),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 27, right: 27, top: 30),
-                      child: SlideAnimation(
-                        delay: 200,
-                        child: TextField(
-                          textAlign: TextAlign.start,
-                          onChanged: onTextChanged,
-                          controller: controller,
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.white,
-                            hintText: 'Search',
-                            prefixIcon: const Icon(Icons.search),
-                            suffixIcon: InkWell(
-                              child: const Icon(Icons.cancel),
-                              onTap: () {
-                                controller.clear();
-                              },
-                            ),
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              ValueListenableBuilder(
-                valueListenable: searchedUsers,
-                builder: (context, List<UserModel> users, _) {
-                  return UsersList(users: users);
-                },
-              ),
-            ],
+    return Stack(
+      children: [
+        Column(
+          children: [
+            // AppBar
+            appBar(),
+
+            //Users list
+            ValueListenableBuilder(
+              valueListenable: searchedUsers,
+              builder: (context, List<UserModel> users, _) {
+                return UsersList(users: users);
+              },
+            ),
+          ],
+        ),
+
+        // Floating action button
+        Positioned(
+          right: 20,
+          bottom: 20,
+          child: SlideAnimation(
+            child: FloatingActionButton(
+              child: const Icon(Icons.add),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => SignUpScreen(isAddUser: true),
+                  ),
+                );
+              },
+            ),
           ),
-          Positioned(
-            right: 20,
-            bottom: 20,
+        ),
+      ],
+    );
+  }
+
+  Widget appBar() {
+    return SizedBox(
+      height: 170,
+      child: AppBar(
+        centerTitle: true,
+        title: const SlideAnimation(
+          delay: 400,
+          child: Text(
+            'Users',
+            style: TextStyle(fontSize: 33, color: Colors.white, fontWeight: FontWeight.w400),
+          ),
+        ),
+        bottom: PreferredSize(
+          preferredSize: Size.zero,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
             child: SlideAnimation(
-              child: FloatingActionButton(
-                child: const Icon(Icons.add),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => SignUpScreen(isAddUser: true),
-                    ),
-                  );
-                },
+              delay: 200,
+              child: TextField(
+                textAlign: TextAlign.start,
+                onChanged: onTextChanged,
+                controller: controller,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: 'Search users',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  prefixIcon: const Icon(Icons.search),
+                  suffixIcon: InkWell(
+                    child: const Icon(Icons.cancel),
+                    onTap: () {
+                      controller.clear();
+                    },
+                  ),
+                ),
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
