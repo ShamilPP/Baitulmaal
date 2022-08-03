e32bd0c20423f7e742c58ae05399da2ae1b3a4a1
ShamilPP <shamilpp4115@gmail.com>
2022-08-03 09:58:22 +0530

    transaction screen appbar fixed

--- lib/view/screens/transactions_screen.dart
+++ lib/view/screens/transactions_screen.dart
@@ -16,67 +16,65 @@ class TransactionScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 100,
-            child: AppBar(
-              backgroundColor: primaryColor,
-              title: const SlideAnimation(
-                delay: 100,
-                child: Text(
-                  'Transactions',
-                  style: TextStyle(fontSize: 23, fontWeight: FontWeight.w400, color: Colors.white),
-                ),
-              ),
-              bottom: const TabBar(
-                isScrollable: true,
-                tabs: [
-                  SlideAnimation(delay: 200, child: Tab(text: 'All')),
-                  SlideAnimation(delay: 300, child: Tab(text: 'Accepted')),
-                  SlideAnimation(delay: 400, child: Tab(text: 'Rejected')),
-                  SlideAnimation(delay: 500, child: Tab(text: 'Not verified')),
-                ],
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: 120,
+          child: AppBar(
+            backgroundColor: primaryColor,
+            title: const SlideAnimation(
+              delay: 100,
+              child: Text(
+                'Transactions',
+                style: TextStyle(fontSize: 23, fontWeight: FontWeight.w400, color: Colors.white),
               ),
             ),
+            bottom: const TabBar(
+              isScrollable: true,
+              tabs: [
+                SlideAnimation(delay: 200, child: Tab(text: 'All')),
+                SlideAnimation(delay: 300, child: Tab(text: 'Accepted')),
+                SlideAnimation(delay: 400, child: Tab(text: 'Rejected')),
+                SlideAnimation(delay: 500, child: Tab(text: 'Not verified')),
+              ],
+            ),
           ),
-          Consumer<PaymentProvider>(builder: (ctx, provider, child) {
-            List<PaymentModel> acceptedList = provider.getPaymentListWithStatus(payments, PaymentStatus.accepted);
-            List<PaymentModel> rejectedList = provider.getPaymentListWithStatus(payments, PaymentStatus.rejected);
-            List<PaymentModel> notVerifiedList = provider.getPaymentListWithStatus(payments, PaymentStatus.notVerified);
-            return Expanded(
-              child: TabBarView(
-                children: [
-                  // All payments tab
-                  PaymentList(
-                    paymentList: payments,
-                    isAdmin: isAdmin,
-                  ),
+        ),
+        Consumer<PaymentProvider>(builder: (ctx, provider, child) {
+          List<PaymentModel> acceptedList = provider.getPaymentListWithStatus(payments, PaymentStatus.accepted);
+          List<PaymentModel> rejectedList = provider.getPaymentListWithStatus(payments, PaymentStatus.rejected);
+          List<PaymentModel> notVerifiedList = provider.getPaymentListWithStatus(payments, PaymentStatus.notVerified);
+          return Expanded(
+            child: TabBarView(
+              children: [
+                // All payments tab
+                PaymentList(
+                  paymentList: payments,
+                  isAdmin: isAdmin,
+                ),
 
-                  // Accepted tab
-                  PaymentList(
-                    paymentList: acceptedList,
-                    isAdmin: isAdmin,
-                  ),
+                // Accepted tab
+                PaymentList(
+                  paymentList: acceptedList,
+                  isAdmin: isAdmin,
+                ),
 
-                  // Rejected tab
-                  PaymentList(
-                    paymentList: rejectedList,
-                    isAdmin: isAdmin,
-                  ),
+                // Rejected tab
+                PaymentList(
+                  paymentList: rejectedList,
+                  isAdmin: isAdmin,
+                ),
 
-                  // Not verified tab
-                  PaymentList(
-                    paymentList: notVerifiedList,
-                    isAdmin: isAdmin,
-                  ),
-                ],
-              ),
-            );
-          })
-        ],
-      ),
+                // Not verified tab
+                PaymentList(
+                  paymentList: notVerifiedList,
+                  isAdmin: isAdmin,
+                ),
+              ],
+            ),
+          );
+        })
+      ],
     );
   }
 }
