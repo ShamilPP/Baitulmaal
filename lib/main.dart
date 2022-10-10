6f174fe0993c504fbe490b41611530e64cfe937e
ShamilPP <shamilpp4115@gmail.com>
2022-10-10 07:57:28 +0530

    project updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
